bd016dd53a762b2b7b133e17413b8431556bffb4
Craig Brown <saidake@qq.com>
2025-02-12 23:05:58 +0800

    [simi-blog] feat: Basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,15 @@ import './App.css';
 
 function App() {
   return <div style={{"backgroundColor":"grey", "height":"100vh"}}>
-    Simi Blog Main Page
+      {/* ---------------------------------------------- Heading */}
+      <div style={{"backgroundColor":"pink", height: "50px"}}>
+        Heading
+      </div>
+      {/* ---------------------------------------------- Content */}
+      <div style={{"backgroundColor":"#ccc","height":"100%","display":"flex"}}>
+        <div style={{"backgroundColor":"#299","flex":"0 1 auto"}}>Directory</div>
+        <div style={{"backgroundColor":"#355","flex":"1 1 auto"}}>Content</div>
+      </div>
   </div>;
 }
 
